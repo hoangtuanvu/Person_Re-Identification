1924e29adf4d5f6fc569d09c2c11c2999a9c2055
hoangtuanvu <hoangvu.kstn@gmail.com>
2019-06-21 09:27:05 +0700

    Remove redundant

--- detection/utils/commons.py
+++ detection/utils/commons.py
@@ -28,18 +28,6 @@ def load_cls_dict(path):
     return cls_dict
 
 
-def model_info(model):
-    """Plots a line-by-line description of a PyTorch model"""
-    n_p = sum(x.numel() for x in model.parameters())  # number parameters
-    n_g = sum(x.numel() for x in model.parameters() if x.requires_grad)  # number gradients
-    print('\n%5s %40s %9s %12s %20s %10s %10s' % ('layer', 'name', 'gradient', 'parameters', 'shape', 'mu', 'sigma'))
-    for i, (name, p) in enumerate(model.named_parameters()):
-        name = name.replace('module_list.', '')
-        print('%5g %40s %9s %12g %20s %10.3g %10.3g' % (
-            i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
-    print('Model Summary: %g layers, %g parameters, %g gradients' % (i + 1, n_p, n_g))
-
-
 def compute_ap(recall, precision):
     """ Compute the average precision, given the recall and precision curves.
     Code originally from https://github.com/rbgirshick/py-faster-rcnn.
